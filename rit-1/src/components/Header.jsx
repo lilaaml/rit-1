ebc5181e4eef3c945417ed93dbd8178adc32e22f
Lila Amelia <lilaamelia77@gmail.com>
2024-06-29 14:16:50 +0700

    initial commit

---
+++ rit-1/src/components/Header.jsx
@@ -0,0 +1,18 @@
+const Header = () => {
+  return (
+    <div className="container-sm">
+        <h3 className="lead fw-bold text-center mt-5">
+            This is Header
+        </h3>
+        <div className="card mt-3">
+            <div className="card-body">
+                <h5 className="card-title">This is card</h5>
+                <p className="card-text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab doloribus aut perferendis aspernatur dolore nobis!</p>
+                <a href="/" className="btn btn-primary">Confirm</a>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Header
